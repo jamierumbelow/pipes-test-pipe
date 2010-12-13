aa0b0e457a5c9bbced843b89ba1dace45aef7717
Jamie Rumbelow <jamie@jamierumbelow.net>
2010-12-13 00:23:36 +0000

    Quick example lib

---
+++ lib/example_pipe.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Pipes
+ * 
+ * A sexy PHP package manager
+ *
+ * @author Jamie Rumbelow
+ * @copyright Copyright (c) 2010 Jamie Rumbelow
+ * @package pipes
+ * @subpackage example_pipe
+ **/
+
+class Example_Pipe {
+	public function some_method() {
+		echo "Blah blah blah";
+	}
+}
